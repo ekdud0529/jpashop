7da0f3192320e105c5651dc637dd61baf3d4c533
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2023-07-04 16:45:40 +0900

    Add section 6-5
    1. 동적 쿼리 생성 방법들 예제 추가
    
    Add #6

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,9 +2,15 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,10 +19,49 @@ public class OrderRepository {
     private final EntityManager em;
 
     public void save(Order order) {
+
         em.persist(order);
     }
 
     public Order findOne(Long id) {
+
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAllByStrim(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // ...
+
+        return em.createQuery( jpql, Order.class) //JPQL
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
+
+    /**
+     * JPA Criteria
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
     }
 }
